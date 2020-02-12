08496c7bc713494a013b04009347b856640da6c3
macarenasaez <makadaira@gmail.com>
2020-02-12 13:26:56 -0300

    encontrar archivos md dentro de un directorio

---
+++ src/index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+const mdLinks = require('./md-links.js');
+const process = require('process');
+const pathNode = require('path');
+const chalk = require('chalk');
+
+let path = process.argv[2]
+//path.resolve para poder convertir en absoluta
+path = pathNode.resolve(path);
+//pat.normalize es para normalizar path en caso de q hayan errores de semanticas 
+path.normalize('NFC');
+
+let options ={
+    stats:false,
+    validate:false,
+}
+
+process.argv.forEach(element =>{
+if(element == "--stats" || element == "-s" ){
+    option.validate = true
+}
+})
+
+mdLinks.mdLinks(path,options).then(res =>{
+    if (options.validate && options.stats){
+        return console.log(chalk.bgblue("Total Links: " + res.total) + "\n" + chalk.bgGreen("Ok Links:" + res.ok) + "\n" + chalk.bgRed("Broken Links: " + res.bronken))
+    }
+
+    if (options.validate){
+        if (res.length === 0){
+            return
+        }
+    }
+})
---
+++ src/md-links.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const marked = require('marked');
+const FileHound = require('filehound');
+const fetch = require('node-fetch');
+
+//FILEHOUND NOS SIRVE PARA ENCONTAR ARCHIVOS MD DENTRO DE UN DIRECTORÍO
+const mdLinks = (path => {
+    return new Promise((resolve, reject) => {
+        FileHound.create()
+            .paths(path)
+            .ext('md')
+            .find()
+            .then(
+                file => {
+                    if (file.length !== 0) {
+                        resolve(file)
+                        console.log(file)
+                    }
+                
+                }
+            )
+            .catch(err => {
+                reject(new Error("Esta ruta no existe"))
+            })
+    })
+})
+//extraer links md
+
+module.exports = {
+    mdLinks
+}
