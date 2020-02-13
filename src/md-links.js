7f22ec61410e63db901c638e3fa1a1d39bcf557b
macarenasaez <makadaira@gmail.com>
2020-02-13 19:49:59 -0300

    leer archivos .md listo y funcionando

--- src/md-links.js
+++ src/md-links.js
@@ -23,8 +23,48 @@ const mdLinks = (path => {
             })
     })
 })
-//extraer links md
 
-module.exports = {
-    mdLinks
-}
+
+//leer archibos .md 
+const readMD =(path =>{
+    return new Promise ((resolve,reject)=>{
+        fs.readFile(path,'utf8', (err,data)=>{
+            if (err){
+                reject(console.log("!Ouch¡ nose a encontrado este archivo: " + userpath))
+            }
+            resolve(data)
+        }) 
+    })
+})
+
+//obtener links de un archivo .md
+const indlinks =(path =>{
+    return new  Promise((resolve,reject) =>{
+        readMD(path).then(res =>{
+            let links =[];
+            const renderer = new marked.Renderer();
+            renderer.link = function(href,title,text){
+
+                if(!href.startsWith("mai:")){
+                    links.push({
+                        //encotrada la url
+                        href:href,
+                        //el texto que aparece acompañado al link
+                        text:text,
+                        //ruta del archivodonde encuentra el links
+                        file:path })
+                   
+                }
+            }
+            marked(res,{renderer:renderer});
+            resolve(links)
+        })
+        .catch(err =>{
+            reject(err)
+        })
+    })
+})
+
+//module.exports = {
+  //  mdLinks
+//}
